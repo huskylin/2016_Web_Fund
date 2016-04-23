ceeb436ce23a937a2b3bc1185e5d24df01b380db
YogaPan <godhand1234567@gmail.com>
2016-04-23 22:58:06 +0800

    Add condition in templates

--- husky_mooc/husky_mooc/views.py
+++ husky_mooc/husky_mooc/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-    husky   = { 'name': '林昱辰', 'age': 20, 'horoscope': 'Scorpio' }
-    yoga    = { 'name': '潘昱嘉', 'age': 20, 'horoscope': 'Aries' }
-    shuohan = { 'name': '高碩亨', 'age': 99, 'horoscope': 'Unknown' }
+    husky   = { 'name': '林昱辰', 'age': 20, 'horoscope': 'Scorpio',
+            'is_handsome': False }
+    yoga    = { 'name': '潘昱嘉', 'age': 20, 'horoscope': 'Aries',
+            'is_handsome': True }
+    shuohan = { 'name': '高碩亨', 'age': 99, 'horoscope': 'Unknown',
+            'is_handsome': True }
     people  = [husky, yoga, shuohan]
     return render_to_response('index.html', locals())

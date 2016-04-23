e596cbede6ec2da0e723454b9d68c30988d70ff4
YogaPan <godhand1234567@gmail.com>
2016-04-23 18:44:19 +0800

    For loop in templates

--- husky_mooc/husky_mooc/views.py
+++ husky_mooc/husky_mooc/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-    husky = { 'name': '林昱辰', 'age': 20, 'horoscope': 'Scorpio' }
+    husky   = { 'name': '林昱辰', 'age': 20, 'horoscope': 'Scorpio' }
+    yoga    = { 'name': '潘昱嘉', 'age': 20, 'horoscope': 'Aries' }
+    shuohan = { 'name': '高碩亨', 'age': 99, 'horoscope': 'Unknown' }
+    people  = [husky, yoga, shuohan]
     return render_to_response('index.html', locals())

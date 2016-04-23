c039a532be20caba3f9a37316474dcd90627d8da
YogaPan <godhand1234567@gmail.com>
2016-04-24 02:50:53 +0800

    Add bower and migration

--- husky_mooc/login_system/models.py
+++ husky_mooc/login_system/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    username = models.CharField(max_length=40)
+    age  = models.DecimalField(max_digits=100, decimal_places=0)
+    desciption = models.CharField(max_length=100, blank=True)
+    is_handsome = models.BooleanField()

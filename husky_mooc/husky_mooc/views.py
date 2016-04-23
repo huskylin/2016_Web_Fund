97033bdc1dfa5a43fb2a7d692cb64390995d9b66
YogaPan <godhand1234567@gmail.com>
2016-04-23 18:19:22 +0800

    Add render method

--- husky_mooc/husky_mooc/views.py
+++ husky_mooc/husky_mooc/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
 
 def index(request):
+    husky = { 'name': '林昱辰', 'age': 20, 'horoscope': 'Scorpio' }
     return render_to_response('index.html', locals())

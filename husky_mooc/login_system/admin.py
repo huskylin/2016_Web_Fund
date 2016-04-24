de89eac277567687cc241f8553c3dbf4163ad456
YogaPan <godhand1234567@gmail.com>
2016-04-24 22:01:14 +0800

    Fix bugs

--- husky_mooc/login_system/admin.py
+++ husky_mooc/login_system/admin.py
@@ -3,14 +3,16 @@ from login_system.models import User, Post
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'age', 'is_handsome', 'date')
-    list_filter  = ('is_handsome',)
+    list_display  = ('username', 'age', 'is_handsome', 'date')
+    list_filter   = ('is_handsome',)
     search_fields = ('username',)
-    ordering = ()
+    ordering      = ('-age', 'date')
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content', 'date')
-    ordering = ('-date')
+    list_display  = ('user', 'content', 'date')
+    search_fielse = ('user',)
+    fields        = ('user', 'content')
+    ordering      = ('date',)
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Post, PostAdmin)

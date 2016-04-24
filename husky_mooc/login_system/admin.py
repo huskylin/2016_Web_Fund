d4078d387c5c7cedddd371a223ea5004e328be3f
YogaPan <godhand1234567@gmail.com>
2016-04-24 21:56:49 +0800

    Add admin

--- husky_mooc/login_system/admin.py
+++ husky_mooc/login_system/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from login_system.models import User, Post
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'age', 'is_handsome', 'date')
+    list_filter  = ('is_handsome',)
+    search_fields = ('username',)
+    ordering = ()
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content', 'date')
+    ordering = ('-date')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)

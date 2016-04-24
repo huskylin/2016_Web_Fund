9c8f0a34d6399da4d61cad79c28cabd94e0a2922
YogaPan <godhand1234567@gmail.com>
2016-04-24 17:31:49 +0800

    change user and post models

--- husky_mooc/login_system/models.py
+++ husky_mooc/login_system/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
-# Create your models here.
-class Member(models.Model):
-    username = models.CharField(max_length=40)
-    age  = models.DecimalField(max_digits=100, decimal_places=0)
-    desciption = models.CharField(max_length=100, blank=True)
+
+class User(models.Model):
+    username    = models.CharField(max_length=50)
+    password    = models.CharField(max_length=50)
+    nickname    = models.CharField(max_length=50)
+    age         = models.DecimalField(max_digits=100, decimal_places=0)
+    desciption  = models.CharField(max_length=100, blank=True)
     is_handsome = models.BooleanField()
+    date        = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Post(models.Model):
+    user    = models.ForeignKey(User)
+    content = models.CharField(max_length=100)
+    date    = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
